190356483d2c3b467fd1dcdeef01c39a10add00e
Alseias <k_kurtulus@hotmail.com>
2017-01-14 17:59:50 +0300

    Added Project
    All project file added excluding temp file

---
+++ Library/Collab/Download/Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Photon.PunBehaviour {
+
+    public void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+    void LoadArena()
+    {
+        if (!PhotonNetwork.isMasterClient)
+        {
+            Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+        }
+        Debug.Log("PhotonNetwork : Loading Level : " + PhotonNetwork.room.playerCount);
+        PhotonNetwork.LoadLevel("Game");
+    }
+    #region Photon Messages
+
+
+    public override void OnPhotonPlayerConnected(PhotonPlayer other)
+    {
+        Debug.Log("OnPhotonPlayerConnected() " + other.name); // not seen if you're the player connecting
+
+
+        if (PhotonNetwork.isMasterClient)
+        {
+            Debug.Log("OnPhotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+
+
+            LoadArena();
+        }
+    }
+
+
+    public override void OnPhotonPlayerDisconnected(PhotonPlayer other)
+    {
+        Debug.Log("OnPhotonPlayerDisconnected() " + other.name); // seen when other disconnects
+
+
+        if (PhotonNetwork.isMasterClient)
+        {
+            Debug.Log("OnPhotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+
+
+            LoadArena();
+        }
+    }
+
+
+    #endregion
+}
