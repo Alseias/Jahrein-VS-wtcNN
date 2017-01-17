febdc596e0897ced6e1d92f1fbff5e731afb262e
Legolhinn <emre.aydn.1996@gmail.com>
2017-01-17 16:09:34 +0300

    JahRageTest
    JahRageAnimation

---
+++ Assets/JahTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class JahTest : MonoBehaviour
+{
+    Animator anim;
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Q))
+        {
+            anim.SetInteger("state", 2);
+        }
+	}
+}
