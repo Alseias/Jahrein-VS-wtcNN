6d737e19fbfb2e3efb4cb8d65e23024fe556aa0a
Alseias <k_kurtulus@hotmail.com>
2017-02-06 22:54:57 +0300

    raycast

---
+++ Assets/Scripts/Players/_UI/Abilities.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abilities : MonoBehaviour {
+
+    public string skillKey;
+    public float coolDown;
+    public float damage;
+    public AudioSource audio;
+}
---
+++ Assets/Scripts/Players/_UI/AbilityCoolDown.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AbilityCoolDown : MonoBehaviour {
+
+    public string abilityButtonAxisName = "Fire1";
+    public Image darkMask;
+    public Text coolDownTextDisplay;
+    public float coolDownDuration,durationTime;
+    public bool itsReady,durationEnd,isPassive,passiveTriggered;
+
+
+    private GameObject weaponHolder;
+    private Image myButtonImage;
+    private AudioSource abilitySource;
+    private float nextReadyTime;
+    private float coolDownTimeLeft,tempDuration;
+    private bool btnTriggered=false;
+    private PlayerController target;
+
+    public void setTarget(PlayerController pc) {
+        target = pc;
+    }
+
+    void Start() {
+        tempDuration = durationTime;
+        AbilityReady();
+    }
+
+    public void Initialize( GameObject weaponHolder) {
+        myButtonImage = GetComponent<Image>();
+        abilitySource = GetComponent<AudioSource>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        bool coolDownComplete = (Time.time > nextReadyTime);
+
+        if(coolDownComplete) {
+            itsReady = true;
+            AbilityReady();
+            if(Input.GetButtonDown(abilityButtonAxisName)&&!isPassive) {
+                btnTriggered = true;
+                ButtonTriggered();
+            }else {
+                if(passiveTriggered) {
+                    passiveTriggered = false;
+                    ButtonTriggered();
+                }
+            }
+        } else {
+            itsReady = false;
+            CoolDown();
+        }
+        
+        if(btnTriggered && !isPassive) {
+            durationTime -= Time.deltaTime;
+            if(durationTime <= 0) {
+                btnTriggered = false;
+                durationEnd = true;
+                durationTime = tempDuration;
+            } else {
+                durationEnd = false;
+            }
+        }
+    }
+    
+    private void AbilityReady() {
+        coolDownTextDisplay.enabled = false;
+        darkMask.enabled = false;
+        
+    }
+
+    private void CoolDown() {
+        coolDownTimeLeft -= Time.deltaTime;
+        float roundedCd = Mathf.Round(coolDownTimeLeft);
+        coolDownTextDisplay.text = roundedCd.ToString();
+        darkMask.fillAmount = (coolDownTimeLeft / coolDownDuration);
+    }
+
+    private void ButtonTriggered() {
+        
+        nextReadyTime = coolDownDuration + Time.time;
+        coolDownTimeLeft = coolDownDuration;
+        darkMask.enabled = true;
+        coolDownTextDisplay.enabled = true;
+
+             
+    }
+}
