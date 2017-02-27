2b95e34d269742a47be3a51cc30b12e65a063c3f
Alseias <k_kurtulus@hotmail.com>
2017-02-27 19:45:01 +0300

    wtcn shuriken

--- Assets/Scripts/Players/wtcNN/shurikenScript.cs
+++ Assets/Scripts/Players/wtcNN/shurikenScript.cs
@@ -11,36 +11,59 @@ public class shurikenScript : MonoBehaviour
     int bulletDamage = 40;
     Rigidbody2D rb;
     public bool faceDir;
-    float direction;
+    public float direction,fDir;
+    public int pvID;
 
     GameObject wtcn;
     void dir(bool _dir) {
-        
-        faceDir = _dir;
+        fDir = -1;
+        if(_dir) {
+            fDir = 1;
+        }
+    }
+
+    void id(int _id) {
+        pvID = _id;
+
     }
     void Start ()
     {
         //wtcn = GameObject.Find("wtcn");
         Destroy(gameObject, 2);
         rb = GetComponent<Rigidbody2D>();
-        //direction = wtcn.GetComponent<wtcnnSkills>().SendDirection();
-        rb.AddForce(new Vector2(direction * 10, transform.position.y), ForceMode2D.Impulse);
-        rb.velocity = speed * (faceDir ? 1 : -1);
+        //rb.AddForce(new Vector2(direction * 10, transform.position.y) * (faceDir ? 1 : -1), ForceMode2D.Impulse);
+        rb.velocity = speed * fDir;
+        //GetComponent<PhotonView>().RPC("setVelocity", PhotonTargets.All);
+
+
     }
+    [PunRPC]
+    void setVelocity() {
+        rb.velocity = speed * fDir;
+
+    }
+    private void Update() {
+        rb.AddForce(transform.right * 10f * fDir);
+
+    }
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("enemy"))
-        {
-            Destroy(gameObject);
-            if (this.gameObject.CompareTag("bullet"))
-            {
-                other.gameObject.GetComponent<Stats>().TakeDamage(bulletDamage);
-            }
-            else if (this.gameObject.CompareTag("shuriken"))
-            {
-                other.gameObject.GetComponent<Stats>().TakeDamage(shurikenDamage);
+        
+        if(other.GetComponent<PhotonView>() != null) {
+            if(other.GetComponent<PhotonView>().viewID != pvID) {
+                if(other.gameObject.CompareTag("enemy") || other.gameObject.CompareTag("Player")) {
+                    Destroy(gameObject);
+                    if(this.gameObject.CompareTag("bullet")) {
+                        other.gameObject.GetComponent<Stats>().TakeDamage(bulletDamage);
+                    } else if(this.gameObject.CompareTag("shuriken")) {
+                        other.gameObject.GetComponent<Stats>().TakeDamage(shurikenDamage);
+                    }
+                }
             }
         }
+        
+
     } 
 }
